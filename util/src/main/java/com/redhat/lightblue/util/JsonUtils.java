557784fe253321b560705b762e0bc81fa8513b62
Burak Serdar <bserdar@redhat.com>
2013-11-11 12:26:35 -0700

    Add factory class to create json objects that can deal with bigdecimals

---
+++ util/src/main/java/com/redhat/lightblue/util/JsonUtils.java
@@ -0,0 +1,23 @@
+package com.redhat.lightblue.util;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+
+public final class JsonUtils {
+
+    public static ObjectMapper getObjectMapper() {
+        ObjectMapper mapper=new ObjectMapper();
+        mapper.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS,true);
+        return mapper;
+    }        
+
+    public static JsonNode json(String s) 
+        throws IOException {
+        return getObjectMapper().readTree(s);
+    }
+
+    private JsonUtils() {}
+}
