6939817e134b61623dda739790a2725c420b296e
Naveen Malik <nmalik@redhat.com>
2014-01-15 17:34:20 -0500

    Updated crud resource to actually do real work

--- rest/crud/src/main/java/com/redhat/lightblue/rest/crud/CrudResource.java
+++ rest/crud/src/main/java/com/redhat/lightblue/rest/crud/CrudResource.java
@@ -1,21 +1,22 @@
 package com.redhat.lightblue.rest.crud;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.redhat.lightblue.DeleteRequest;
+import com.redhat.lightblue.FindRequest;
+import com.redhat.lightblue.InsertionRequest;
+import com.redhat.lightblue.Response;
+import com.redhat.lightblue.SaveRequest;
+import com.redhat.lightblue.UpdateRequest;
 import com.redhat.lightblue.crud.CrudManager;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.google.gson.Gson;
-import com.redhat.lightblue.metadata.EntityMetadata;
-import com.redhat.lightblue.metadata.MetadataStatus;
-import com.redhat.lightblue.metadata.Version;
 import com.redhat.lightblue.util.Error;
 import com.redhat.lightblue.util.JsonUtils;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 
 /**
  * Simple service to test out NewRelic custom metrics.
@@ -27,21 +28,11 @@ public class CrudResource {
     public static final String ERR_REST_ERROR = "REST_ERROR";
 
     @GET
-    @Path("/{entity}/{version}")
-    public String getMetadata(@PathParam("entity") String entityName, @PathParam("version") String entityVersion) {
+    @Path("/find")
+    public String find(String data) {
         try {
-            if (entityName == null) {
-                throw Error.get(ERR_REST_ERROR, "Entity name not provided");
-            }
-            if (entityVersion == null) {
-                throw Error.get(ERR_REST_ERROR, "Entity version not provided");
-            }
-
-            // get the data
-            EntityMetadata em = CrudManager.getMetadata().getEntityMetadata(entityName, entityVersion);
-
-            // convert to json and return
-            return CrudManager.getJSONParser().convert(em).toString();
+            Response r = CrudManager.getMediator().find(FindRequest.fromJson((ObjectNode) JsonUtils.json(data.toString())));
+            return r.toJson().toString();
         } catch (Error e) {
             Logger.getLogger(CrudResource.class.getName()).log(Level.SEVERE, null, e);
             return e.toJson().toString();
@@ -51,4 +42,63 @@ public class CrudResource {
         }
     }
 
+    @PUT
+    @Path("/insert")
+    public String insert(String data) {
+        try {
+            Response r = CrudManager.getMediator().insert(InsertionRequest.fromJson((ObjectNode) JsonUtils.json(data.toString())));
+            return r.toJson().toString();
+        } catch (Error e) {
+            Logger.getLogger(CrudResource.class.getName()).log(Level.SEVERE, null, e);
+            return e.toJson().toString();
+        } catch (Exception e) {
+            Logger.getLogger(CrudResource.class.getName()).log(Level.SEVERE, null, e);
+            return Error.get(ERR_REST_ERROR).toJson().toString();
+        }
+    }
+
+    @POST
+    @Path("/update")
+    public String update(String data) {
+        try {
+            Response r = CrudManager.getMediator().update(UpdateRequest.fromJson((ObjectNode) JsonUtils.json(data.toString())));
+            return r.toJson().toString();
+        } catch (Error e) {
+            Logger.getLogger(CrudResource.class.getName()).log(Level.SEVERE, null, e);
+            return e.toJson().toString();
+        } catch (Exception e) {
+            Logger.getLogger(CrudResource.class.getName()).log(Level.SEVERE, null, e);
+            return Error.get(ERR_REST_ERROR).toJson().toString();
+        }
+    }
+
+    @PUT
+    @Path("/save")
+    public String save(String data) {
+        try {
+            Response r = CrudManager.getMediator().save(SaveRequest.fromJson((ObjectNode) JsonUtils.json(data.toString())));
+            return r.toJson().toString();
+        } catch (Error e) {
+            Logger.getLogger(CrudResource.class.getName()).log(Level.SEVERE, null, e);
+            return e.toJson().toString();
+        } catch (Exception e) {
+            Logger.getLogger(CrudResource.class.getName()).log(Level.SEVERE, null, e);
+            return Error.get(ERR_REST_ERROR).toJson().toString();
+        }
+    }
+
+    @DELETE
+    @Path("/delete")
+    public String delete(String data) {
+        try {
+            Response r = CrudManager.getMediator().delete(DeleteRequest.fromJson((ObjectNode) JsonUtils.json(data.toString())));
+            return r.toJson().toString();
+        } catch (Error e) {
+            Logger.getLogger(CrudResource.class.getName()).log(Level.SEVERE, null, e);
+            return e.toJson().toString();
+        } catch (Exception e) {
+            Logger.getLogger(CrudResource.class.getName()).log(Level.SEVERE, null, e);
+            return Error.get(ERR_REST_ERROR).toJson().toString();
+        }
+    }
 }
