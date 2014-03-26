b4cf932aa67bb3c61fa5d407dfd7e21d94ae087d
Luan Cestari <luan.cestari@gmail.com>
2014-03-26 10:30:52 -0300

    Create another package to Crud configuration

---
+++ config/crud/src/test/java/com/redhat/lightblue/metadata/test/DatabaseMetadata.java
@@ -0,0 +1,74 @@
+/*
+ Copyright 2013 Red Hat, Inc. and/or its affiliates.
+
+ This file is part of lightblue.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.redhat.lightblue.metadata.test;
+
+import com.redhat.lightblue.Response;
+import com.redhat.lightblue.metadata.EntityMetadata;
+import com.redhat.lightblue.metadata.Metadata;
+import com.redhat.lightblue.metadata.MetadataStatus;
+import com.redhat.lightblue.metadata.Version;
+
+/**
+ *
+ * @author nmalik
+ */
+public class DatabaseMetadata implements Metadata {
+
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseMetadata() {
+
+    }
+
+    @Override
+    public EntityMetadata getEntityMetadata(String entityName, String version, boolean forceVersion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String[] getEntityNames() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Version[] getEntityVersions(String entityName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createNewMetadata(EntityMetadata md) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setMetadataStatus(String entityName, String version, MetadataStatus newStatus, String comment) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Response getDependnecies(String entityName, String version) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Response getAccess(String entityName, String version) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
