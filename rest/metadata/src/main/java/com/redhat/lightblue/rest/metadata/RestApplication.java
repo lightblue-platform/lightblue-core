2c2b3640439bd5575ba2c538614b20f74a58dc56
Naveen Malik <nmalik@redhat.com>
2014-03-31 10:59:51 -0400

    Cleanup rest/crud in review

--- rest/metadata/src/main/java/com/redhat/lightblue/rest/metadata/RestApplication.java
+++ rest/metadata/src/main/java/com/redhat/lightblue/rest/metadata/RestApplication.java
@@ -25,7 +25,6 @@ import java.util.Set;
 import javax.ws.rs.core.Application;
 
 public class RestApplication extends Application {
-
     @Override
     public Set<Class<?>> getClasses() {
         return new HashSet<Class<?>>(Arrays.asList(MetadataResource.class));
