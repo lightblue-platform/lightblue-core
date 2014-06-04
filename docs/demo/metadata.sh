ff768ae9ff119633431f1709a791660471017c64
Naveen Malik <nmalik@redhat.com>
2014-06-04 19:29:19 -0400

    Updates to metadata pyresttest stuff

--- docs/demo/metadata.sh
+++ docs/demo/metadata.sh
@@ -2,4 +2,4 @@
 
 # requires resttest (https://github.com/jewzaam/pyresttest) and argparse module
 
-python -c "import resttest; resttest.main('http://b09.cos.redhat.com:8080', 'metadata-pyresttest.yaml')"
+python -c "import resttest; resttest.main('http://b09.cos.redhat.com:8080', 'metadata-pyresttest.yaml', 'debug')"
