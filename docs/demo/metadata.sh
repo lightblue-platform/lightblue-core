d4860ee94c6b34388324708afa6a316bd738c477
Naveen Malik <nmalik@redhat.com>
2014-06-05 14:53:48 -0400

    Updates to metadata pyresttest stuff again

--- docs/demo/metadata.sh
+++ docs/demo/metadata.sh
@@ -3,9 +3,15 @@
 # requires resttest (https://github.com/jewzaam/pyresttest) and argparse module
 
 LOGGING_LEVEL="debug"
+export ENTITY_NAME="nmalik-$(date +'%Y%m%d%H%M%S')";
+export ENTITY_VERSION_1="1.0.0";
+export ENTITY_VERSION_2="2.0.0"
 
-if [ "x$1" != "x" ]; then
-    LOGGING_LEVEL=$1
-fi
+echo "Running tests for new entity: $ENTITY_NAME"
 
-python -c "import resttest; resttest.main('http://b09.cos.redhat.com:8080', 'metadata-pyresttest-basic.yaml', '$LOGGING_LEVEL')"
+python -c "import resttest; resttest.main('http://b09.cos.redhat.com:8080', 'metadata-pyresttest.yaml', '$LOGGING_LEVEL')"
+echo $?
+
+unset ENTITY_NAME
+unset ENTITY_VERSION_1
+unset ENTITY_VERSION_2
