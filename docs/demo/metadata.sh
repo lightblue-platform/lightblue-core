7393f7b2b5d2213c6d02fecc4452b4660f00af33
Naveen Malik <nmalik@redhat.com>
2014-06-05 10:22:39 -0400

    Updates to metadata pyresttest, added 'basic' test that just does PUT of new metadata

--- docs/demo/metadata.sh
+++ docs/demo/metadata.sh
@@ -2,4 +2,10 @@
 
 # requires resttest (https://github.com/jewzaam/pyresttest) and argparse module
 
-python -c "import resttest; resttest.main('http://b09.cos.redhat.com:8080', 'metadata-pyresttest.yaml', 'debug')"
+LOGGING_LEVEL="debug"
+
+if [ "x$1" != "x" ]; then
+    LOGGING_LEVEL=$1
+fi
+
+python -c "import resttest; resttest.main('http://b09.cos.redhat.com:8080', 'metadata-pyresttest-basic.yaml', '$LOGGING_LEVEL')"
