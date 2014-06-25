77124e3406508fafa0655bceb99eb47fc62d62cf
skavanagh <sean.p.kavanagh6@gmail.com>
2014-06-25 16:59:32 -0400

    Added new UID datatype
    Generates random UID if field exists and is empty or null

--- lightblue-core/metadata/src/main/java/com/redhat/lightblue/metadata/types/DefaultTypes.java
+++ lightblue-core/metadata/src/main/java/com/redhat/lightblue/metadata/types/DefaultTypes.java
@@ -45,5 +45,6 @@ public final class DefaultTypes implements TypeResolver {
         typeMap.put(ArrayType.NAME, ArrayType.TYPE);
         typeMap.put(ObjectType.NAME, ObjectType.TYPE);
         typeMap.put(ReferenceType.NAME, ReferenceType.TYPE);
+        typeMap.put(UIDType.NAME, UIDType.TYPE);
     }
 }
