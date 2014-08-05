80685beca0919dfc370c52e5ac1f219c9e7d6571
Burak Serdar <bserdar@redhat.com>
2014-08-05 11:40:42 -0600

    Add identity constraint

---
+++ lightblue-core/metadata/src/main/java/com/redhat/lightblue/metadata/constraints/IdentityConstraint.java
@@ -0,0 +1,42 @@
+/*
+ Copyright 2013 Red Hat, Inc. and/or its affiliates.
+
+ This file is part of lightblue.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.redhat.lightblue.metadata.constraints;
+
+import java.io.Serializable;
+
+import com.redhat.lightblue.metadata.FieldConstraint;
+import com.redhat.lightblue.metadata.Type;
+
+/**
+ * Field is part of identityconstraint
+ */
+public class IdentityConstraint implements FieldConstraint, Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    public static final String IDENTITY = "identity";
+
+    public String getType() {
+        return IDENTITY;
+    }
+
+    public boolean isValidForFieldType(Type fieldType) {
+        return true;
+    }
+}
