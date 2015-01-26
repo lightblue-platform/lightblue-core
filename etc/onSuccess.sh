9a8b6603455314f3b9e091a4e2749f7db6f90300
Luan Cestari <luan.cestari@gmail.com>
2015-01-26 11:41:15 -0200

    Fix etc/onSuccess.sh (3)

--- etc/onSuccess.sh
+++ etc/onSuccess.sh
@@ -2,7 +2,7 @@
 
 echo "ofter_success script"
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_JDK_VERSION" == "openjdk7" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_JDK_VERSION" == "openjdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "DEPLOY MASTER TRAVIS BUILD"
     echo "Current directory is $(pwd)"
     mvn clean deploy -DskipTests;
