e36568d7ac2c2e4a56fbd4c9fee6b146c1822dc2
Luan Cestari <luan.cestari@gmail.com>
2015-01-26 11:46:24 -0200

    Typo

--- etc/onSuccess.sh
+++ etc/onSuccess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "ofter_success script"
+echo "after_success script"
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_JDK_VERSION" == "openjdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "DEPLOY MASTER TRAVIS BUILD"
