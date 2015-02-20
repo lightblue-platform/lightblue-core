1de8ace787a061f22639943830efef5a6bbe83ef
Dennis Crissman <dcrissma@redhat.com>
2015-02-20 15:56:25 -0500

    create utility for creating instance of LightblueFactory for testing purposes

---
+++ test/src/main/java/com/redhat/lightblue/test/LightblueUtil.java
@@ -0,0 +1,62 @@
+/*
+ Copyright 2015 Red Hat, Inc. and/or its affiliates.
+
+ This file is part of lightblue.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.redhat.lightblue.test;
+
+import static com.redhat.lightblue.util.test.AbstractJsonNodeTest.loadJsonNode;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import com.redhat.lightblue.config.DataSourcesConfiguration;
+import com.redhat.lightblue.config.JsonTranslator;
+import com.redhat.lightblue.config.LightblueFactory;
+import com.redhat.lightblue.metadata.EntityMetadata;
+import com.redhat.lightblue.metadata.Metadata;
+
+public final class LightblueUtil {
+
+    /**
+     * Creates an instance of the {@link LightblueFactory}.
+     * @param datasourcesResourcePath - path to datasources.json
+     * @param metadataResourcePaths - path to any and all *-metadata.json you'd like {@link LightblueFactory} to know about
+     * @return an instance of {@link LightblueFactory}.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     * @throws InstantiationException
+     */
+    public static LightblueFactory createLightblueFactory(String datasourcesResourcePath, String... metadataResourcePaths)
+            throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException{
+        LightblueFactory lightblueFactory = new LightblueFactory(
+                new DataSourcesConfiguration(loadJsonNode(datasourcesResourcePath)));
+
+        JsonTranslator tx = lightblueFactory.getJsonTranslator();
+
+        Metadata metadata = lightblueFactory.getMetadata();
+        for(String metadataResourcePath : metadataResourcePaths){
+            metadata.createNewMetadata(tx.parse(EntityMetadata.class, loadJsonNode(metadataResourcePath)));
+        }
+
+        return lightblueFactory;
+    }
+
+    private LightblueUtil(){}
+}
