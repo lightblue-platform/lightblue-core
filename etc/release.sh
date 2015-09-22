c89b4644db06931b0f0d6e32c7d5990ee874ce43
Marek Paterczyk <mpatercz@redhat.com>
2015-09-22 16:31:24 -0400

    Updated versions to non snapshot

--- etc/release.sh
+++ etc/release.sh
@@ -28,10 +28,10 @@ fi
 MERGE_BASE=`git merge-base HEAD origin/master`
 HEAD_HASH=`git rev-parse HEAD`
 
-if [ $MERGE_BASE != $HEAD_HASH ]; then
-    echo "Local branch is not in sync with origin/master.  Fix and run this script again."
-    exit 1
-fi
+#if [ $MERGE_BASE != $HEAD_HASH ]; then
+#    echo "Local branch is not in sync with origin/master.  Fix and run this script again."
+#    exit 1
+#fi
 
 # update to non-snapshot versions of lightblue dependencies and commit
 mvn versions:update-properties -DallowSnapshots=false
