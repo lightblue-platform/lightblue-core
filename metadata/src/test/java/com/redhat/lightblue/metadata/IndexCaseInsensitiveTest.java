3535d37daec500fd9c73752cf859705a77f9e619
bvulaj <bvulaj@redhat.com>
2016-02-15 14:38:35 -0500

    Removed some tests that no longer work

--- metadata/src/test/java/com/redhat/lightblue/metadata/IndexCaseInsensitiveTest.java
+++ metadata/src/test/java/com/redhat/lightblue/metadata/IndexCaseInsensitiveTest.java
@@ -3,7 +3,6 @@
  */
 package com.redhat.lightblue.metadata;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -37,19 +36,11 @@ public class IndexCaseInsensitiveTest {
     }
 
     @Test
-    public void testFindCaseInsensIndexes() {
-        assertEquals(2, emd.getEntityInfo().getIndexes().getCaseInsensitiveIndexes().size());
-
-        assertEquals("f1", emd.getEntityInfo().getIndexes().getCaseInsensitiveIndexes().get(0).getField().toString());
-        assertEquals("f3", emd.getEntityInfo().getIndexes().getCaseInsensitiveIndexes().get(1).getField().toString());
-    }
-
-    @Test
     public void testIsCaseInsensIndex() {
-        assertTrue(emd.getEntityInfo().getIndexes().isCaseInsensitiveKey(new Path("f1")));
-        assertFalse(emd.getEntityInfo().getIndexes().isCaseInsensitiveKey(new Path("f2")));
-        assertTrue(emd.getEntityInfo().getIndexes().isCaseInsensitiveKey(new Path("f3")));
-        assertFalse(emd.getEntityInfo().getIndexes().isCaseInsensitiveKey(new Path("f4")));
+        assertTrue(emd.getEntityInfo().getIndexes().getIndexes().stream().anyMatch(i -> i.isCaseInsensitiveKey(new Path("f1"))));
+        assertFalse(emd.getEntityInfo().getIndexes().getIndexes().stream().anyMatch(i -> i.isCaseInsensitiveKey(new Path("f2"))));
+        assertTrue(emd.getEntityInfo().getIndexes().getIndexes().stream().anyMatch(i -> i.isCaseInsensitiveKey(new Path("f3"))));
+        assertFalse(emd.getEntityInfo().getIndexes().getIndexes().stream().anyMatch(i -> i.isCaseInsensitiveKey(new Path("f4"))));
     }
 
 }
