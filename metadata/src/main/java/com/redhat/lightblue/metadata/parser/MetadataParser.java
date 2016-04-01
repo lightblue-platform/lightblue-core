6b90dffbaa590040d11c59ab2800599271ab984a
Dennis Crissman <dcrissma@redhat.com>
2016-04-01 10:55:54 -0400

    minor fix

--- metadata/src/main/java/com/redhat/lightblue/metadata/parser/MetadataParser.java
+++ metadata/src/main/java/com/redhat/lightblue/metadata/parser/MetadataParser.java
@@ -24,11 +24,10 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
-import java.util.Arrays;;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
