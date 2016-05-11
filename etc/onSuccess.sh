56e4636babdfb2fbfa1f3e67d5121f87424cba36
Burak Serdar <bserdar@redhat.com>
2016-05-10 22:11:58 -0600

    Also deploy v2-head artifacts

--- etc/onSuccess.sh
+++ etc/onSuccess.sh
@@ -2,8 +2,11 @@
 
 echo "after_success script"
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo "DEPLOY MASTER TRAVIS BUILD"
-    echo "Current directory is $(pwd)"
-    mvn clean deploy -DskipTests;
+if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "v2-head" ]; then
+     if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+         echo "DEPLOY MASTER TRAVIS BUILD"
+         echo "Current directory is $(pwd)"
+         mvn clean deploy -DskipTests;
+     fi
 fi
+
