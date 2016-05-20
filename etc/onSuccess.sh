48ed3181d8f06cbf6c1b3b6b4956f96579aab9cb
Burak Serdar <bserdar@redhat.com>
2016-05-20 08:13:30 -0600

    Don't push from v2-head any more

--- etc/onSuccess.sh
+++ etc/onSuccess.sh
@@ -2,7 +2,7 @@
 
 echo "after_success script"
 
-if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "v2-head" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
      if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
          echo "DEPLOY MASTER TRAVIS BUILD"
          echo "Current directory is $(pwd)"
