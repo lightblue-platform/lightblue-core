0c02db1c530944046bceb5e51ce3d2a4d08beb2a
Burak Serdar <bserdar@redhat.com>
2016-10-06 09:16:35 -0600

    Fix #699: Array element id checker validates that array element ids are unique

--- crud/src/main/java/com/redhat/lightblue/crud/validator/ArrayElementIdChecker.java
+++ crud/src/main/java/com/redhat/lightblue/crud/validator/ArrayElementIdChecker.java
@@ -19,6 +19,7 @@
 package com.redhat.lightblue.crud.validator;
 
 import java.util.List;
+import java.util.HashSet;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.redhat.lightblue.crud.ConstraintValidator;
@@ -26,13 +27,15 @@ import com.redhat.lightblue.crud.CrudConstants;
 import com.redhat.lightblue.crud.FieldConstraintDocChecker;
 import com.redhat.lightblue.metadata.FieldConstraint;
 import com.redhat.lightblue.metadata.FieldTreeNode;
+import com.redhat.lightblue.metadata.Type;
 import com.redhat.lightblue.util.Error;
 import com.redhat.lightblue.util.JsonDoc;
 import com.redhat.lightblue.util.Path;
+import com.redhat.lightblue.util.KeyValueCursor;
 
 /**
  * element identity fields are required. This constraint checker makes sure they
- * are present in the doc
+ * are present in the doc, and unique
  */
 public class ArrayElementIdChecker implements FieldConstraintDocChecker {
 
@@ -45,6 +48,24 @@ public class ArrayElementIdChecker implements FieldConstraintDocChecker {
         List<Path> errors = RequiredChecker.getMissingFields(fieldMetadataPath, doc);
         for (Path x : errors) {
             validator.addDocError(Error.get(CrudConstants.ERR_REQUIRED, x.toString()));
+        }
+        // Check uniqueness
+        // There are two ways to interpret this:
+        //   1) Element identity is unique within a doc
+        //   2) Element identity is unique within an array
+        // The difference between 1 and 2 is that if an array occurs more than once in a document
+        // (meaning the array is inside an array), then do we allow element identities to repeat for
+        // different arrays? Although #2 makes more sense to some, #1 is more useful and easier to implement,
+        // so we do that.
+        KeyValueCursor<Path,JsonNode> iterator=doc.getAllNodes(fieldMetadataPath);
+        HashSet<Object> values=new HashSet<>();
+        Type t=fieldMetadata.getType();
+        while(iterator.hasNext()) {
+            iterator.next();
+            Path fieldName=iterator.getCurrentKey();
+            JsonNode value=iterator.getCurrentValue();
+            if(!values.add(t.fromJson(value)))
+                validator.addDocError(Error.get(CrudConstants.ERR_DUPLICATE_ARRAY_ELEMENT_ID,value==null?"null":value.asText()));
         }
     }
 }
