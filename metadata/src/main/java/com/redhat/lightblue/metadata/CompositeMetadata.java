65a09d52cc5cfb031b27dac7a58c0c26861e7b22
Burak Serdar <bserdar@redhat.com>
2017-03-24 13:40:01 -0600

    Minor tweaks after review

--- metadata/src/main/java/com/redhat/lightblue/metadata/CompositeMetadata.java
+++ metadata/src/main/java/com/redhat/lightblue/metadata/CompositeMetadata.java
@@ -444,9 +444,10 @@ public class CompositeMetadata extends EntityMetadata {
                                 gmd);
                         path.pop();
                     } else {
-                        newElement = new SimpleArrayElement(((SimpleArrayElement) sourceEl).getType());
-                        ((SimpleArrayElement)newElement).setConstraints( ((SimpleArrayElement)sourceEl).getConstraints());
-                        LOGGER.debug("Constraints:{}",((SimpleArrayElement)newElement).getConstraints());
+                        SimpleArrayElement simpleElement=new SimpleArrayElement(((SimpleArrayElement) sourceEl).getType());
+                        simpleElement.setConstraints(((SimpleArrayElement)sourceEl).getConstraints());
+                        LOGGER.debug("Constraints:{}",simpleElement.getConstraints());
+                        newElement=simpleElement;
                     }
                     newElement.getProperties().putAll(sourceEl.getProperties());
                     ArrayField newField = new ArrayField(field.getName(), newElement);
